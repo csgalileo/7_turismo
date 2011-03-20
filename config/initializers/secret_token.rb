4954654edc270b877c8a6ea6455f5e55d0ea12a7
Cesar Ocaña <celuocme@galileo.edu>
2011-03-20 12:12:16 -0600

    El Commit Inicial del Proyecto TuriGuate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TuriGuate::Application.config.secret_token = 'a07efad9d411815b5c49d3597951ee0d54e33369cff89bdbbd10f2d5c0e8befb9cc93c87cd9a147df00771b09106ae51cb3f00d4c38cad2d50cf50d1df2e05b6'
