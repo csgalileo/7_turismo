b5d7d1555bff0432514fa724d1235af4ecc03d96
Cesar Ocaña <celuocme@galileo.edu>
2011-05-28 16:37:01 -0600

    importacion del menu

---
+++ public/javascripts/navmenu.js
@@ -0,0 +1,5 @@
+			document.write('<li class="current_page_item"><a href="/">Inicio</a></li>');
+			document.write('<li><a href="/activities">Actividades</a></li>');
+			document.write('<li><a href="/locations">Ubicaciones</a></li>');
+			document.write('<li><a href="/events">Eventos</a></li>');			
+			document.write('<li><a href="/services">Servicios</a></li>');
